2cd42808ae90cf69327ce27f7cba28e2fc2b77cf
BaeJuh <ghkslqlsl04@gmail.com>
2025-01-13 15:57:23 +0900

    update

--- tsStart.js
+++ tsStart.js
@@ -1,3 +1,33 @@
 "use strict";
 const multiply = (x, y) => x * y;
-console.log(multiply(3, 7));
+// console.log(multiply(3, 7));
+var Color;
+(function (Color) {
+    Color[Color["red"] = 1] = "red";
+    Color[Color["green"] = 2] = "green";
+    Color[Color["blue"] = 4] = "blue";
+})(Color || (Color = {}));
+const c1 = Color.red;
+console.log(c1);
+const c2 = Color.green;
+console.log(c2);
+const c3 = Color.blue;
+console.log(c3);
+const warning = () => {
+    console.log("Warning");
+};
+const neverFunc = () => {
+    throw new Error;
+};
+// const str: string = String("hello");
+// console.log(str);
+// const canvas: HTMLElement = document.getElementById("canvas");
+const arr = [1, "hello"];
+console.log(arr);
+let arr2 = "str: arr2";
+console.log(arr2);
+arr2 = 2;
+console.log(arr2);
+const [foo, bar, baz] = [1, "str", 3];
+const [x, y, z] = [10, 11, 12];
+console.log(x, y, z);
--- tsStart.ts
+++ tsStart.ts
@@ -1,3 +1,46 @@
 const multiply = (x: number, y: number): number => x * y;
 
-console.log(multiply(3, 7));
+// console.log(multiply(3, 7));
+
+
+enum Color { // 숫자, 문자열
+    red = 1, green = 2, blue = 4
+}
+
+const c1: Color = Color.red;
+console.log(c1);
+
+const c2: Color = Color.green;
+console.log(c2);
+
+const c3: Color = Color.blue;
+console.log(c3);
+
+const warning = (): void => {
+    console.log("Warning");
+}
+
+const neverFunc = (): never => {
+    throw new Error;
+}
+
+// const str: string = String("hello");
+// console.log(str);
+
+// const canvas: HTMLElement = document.getElementById("canvas");
+
+const arr: Array<number|string>|null = [1, "hello"];
+console.log(arr);
+
+let arr2: number|string = "str: arr2";
+
+console.log(arr2);
+
+arr2 = 2;
+
+console.log(arr2);
+
+const [foo, bar, baz]: [number, string, number] = [1, "str", 3];
+const [x, y, z]: number[] = [10, 11, 12];
+
+console.log(x, y, z);

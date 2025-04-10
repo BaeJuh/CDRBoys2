4a3d1d9653b1baa5ca35a66eb4b93e22e1f78a5d
BaeJuh <ghkslqlsl04@gmail.com>
2025-04-10 11:21:14 +0900

    update

--- class_0409/AMclass/vite-project/src/pages/Gallery.jsx
+++ class_0409/AMclass/vite-project/src/pages/Gallery.jsx
@@ -4,14 +4,16 @@ import { motion } from 'framer-motion';
 import "./Gallery.css";
 
 const Gallery = () => {
+    const [dataImg, setDataImg] = useState([]);
 
     useEffect(() => {
         const fetchImg = async () => {
             try {
                 const res = await fetch("data/data.json");
-                const data = res.json();
+                const data = await res.json();
+                console.log(data);
+                setDataImg(data);
 
-                
             } catch (err) {
                 console.log(err);
             }
@@ -20,8 +22,20 @@ const Gallery = () => {
     }, []);
 
     return (
-        <div>
-
+        <div className="galleryOutBox">
+            <div className="scrollImg">
+                <div className="scrollTrack">
+                    {
+                        [...dataImg, dataImg].map((img, i) => (
+                            <div key={i} className="scrollIndex">
+                                <div>{img.title}</div>
+                                <img src={img.src} alt={img.title} />
+                                <div>{img.dis}</div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         </div>
     );
 }

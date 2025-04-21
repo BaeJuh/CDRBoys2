70cb6c18f7aadeb07c61bc74a429f55f5dd149c0
BaeJuh <ghkslqlsl04@gmail.com>
2025-04-21 11:20:16 +0900

    0421 upload

---
+++ class_0421/front/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { React, useState } from 'react';
+import { Link } from 'react-router-dom';
+import "../styles/Navbar.scss";
+
+const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+    return (
+        <header className="navbar">
+            <div className="logo">
+                <Link to="/">MY5</Link>
+            </div>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/guestbook">Guest Book</Link></li>
+                </ul>
+            </nav>
+            <div className="hamburger" onClick={() => { toggleMenu(); }}>
+                &#9776;
+            </div>
+            {
+                isMenuOpen && (
+                    <div className="mobile-menu">
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/guestbook">Guest Book</Link></li>
+                        </ul>
+                    </div>
+                )
+            }
+        </header>
+    );
+}
+
+export default Navbar;
---
+++ class_0421/front/src/pages/Guestbook.jsx
@@ -0,0 +1,8 @@
+const Guestbook = () => {
+    return (
+        <>
+        </>
+    );
+}
+
+export default Guestbook
